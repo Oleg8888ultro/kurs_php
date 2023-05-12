7ee91f306a155e2d76729162be44eaff0735fa9f
Oleg <8888kb8888@gmail.com>
2023-05-12 15:58:51 +0300

    функции

---
+++ continuation15/funcs.php
@@ -0,0 +1,10 @@
+<?php
+function sum(&$a, $b)
+{
+    echo $a + $b;
+    echo '<br>';
+    $a = 100;
+
+}
+
+?>
---
+++ continuation15/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>function15</title>
+</head>
+
+<body>
+
+
+</body>
+
+</html>
+
+<?php error_reporting(-1);
+
+require_once 'funcs.php';
+
+$nums = [1, 2, 3];
+$names = ['Ivanov', 'Petrov'];
+
+echo count($nums) . "<br>";
+echo count($names) . "<br>";
+
+
+
+// $x = 50;
+// $y = 50;
+
+// sum(5, 7);
+// sum(10, 5);
+// sum(5, 10);
+// sum($x, $y);
+
+
+
+$a = 5;
+$b = 10;
+
+echo $a;
+
+sum($a, $b);
+
+echo $a;
+?>
