7b963194c7c28e1a5e90dc168a543713748b1d37
Oleg <8888kb8888@gmail.com>
2023-05-28 19:53:12 +0300

    защита

--- function20/index.php
+++ function20/index.php
@@ -37,4 +37,21 @@ $str = mb_strtoupper($str);
 echo $str; // Выведет У МЭРИ БЫЛ МАЛЕНЬКИЙ ЯГНЕНОК И ОНА ЕГО ОЧЕНЬ ЛЮБИЛА
 
 echo '<br>';
+
+$str = 'Привет, мир!';
+echo mb_substr($str, 0, 6, 'utf-8');
+echo '<br>';
+echo mb_substr($str, 8, 3, 'utf-8');
+echo '<br>';
+echo mb_substr($str, -4, 3, 'utf-8');
+echo '<br>';
+
+$str = '&amp;<h1><i>Привет</i>! Меня зовут <b>Вася</b>!</h1><iframe src="http://webformyself.com" height="100%" width="100%"></iframe><script>alert(\'XSS\')</script>';
+echo htmlspecialchars($str, ENT_QUOTES, 'utf-8', false);
+// echo $str;
+
+$str = '&amp;&lt;h1&gt;&lt;i&gt;Привет&lt;/i&gt;! Меня зовут &lt;b&gt;Вася&lt;/b&gt;!&lt;/h1&gt;&lt;iframe src=&quot;http://webformyself.com&quot; height=&quot;100%&quot; width=&quot;100%&quot;&gt;&lt;/iframe&gt;&lt;script&gt;alert(&#039;XSS&#039;)&lt;/script&gt;';
+echo htmlspecialchars_decode($str);
+
+
 ?>
