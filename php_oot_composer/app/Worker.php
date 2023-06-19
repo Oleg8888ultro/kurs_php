de30f8b45c590c822e816f4bb2b552ae1359b275
Oleg <8888kb8888@gmail.com>
2023-06-19 19:39:14 +0300

    worker

---
+++ php_oot_composer/app/Worker.php
@@ -0,0 +1,15 @@
+<?php
+
+class Worker
+{
+    public string $name;
+    public int $age;
+    public array $hous;
+
+
+    public function work()
+    {
+        print_r('im woking Hello');
+    }
+
+}
